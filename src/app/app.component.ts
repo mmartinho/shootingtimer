3dfbeeeb8fc5f231f0471cce7e7eed3f00d34906
marcus.martinho <marcus.martinho@tjam.jus.br>
2021-10-07 10:41:55 -0400

    Adicionada Google Analytics TAG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shootingtimer';
+
+  /**
+   * @param router 
+   */
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        gtag('config', 'G-2HHXMHV4WE', {'page_path' : event.urlAfterRedirects});  
+      }
+    });
+  }
 }
